42c75356738e47155bd654b27aa1440a9452bee4
Yakumo <lendis12345@gmail.com>
2021-11-06 01:20:17 +0100

    Added config
    Defines the different messages we can exchange in order to make requests and receive responses

---
+++ config.js
@@ -0,0 +1,14 @@
+export const requests = {
+    ADD_TAB: "add-tab",
+    GET_TAB: "get-tab",
+    GET_ALL: "get-all",
+    CLEAR_ALL: "clear-all",
+}
+
+export const responses = {
+    SUCCESS: 420,
+    FAILURE: 69
+}
+
+Object.freeze(requests);
+Object.freeze(responses);
